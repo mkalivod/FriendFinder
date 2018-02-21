de3e902b6871908b65a7ffcc481795569ea84aad
Madison Kalivoda <mkalivoda07@gmail.com>
2018-02-21 11:15:52 -0700

    cleaned up files - still need to fix routing

---
+++ app/data/friends.js
@@ -0,0 +1,58 @@
+
+var friends = [{
+    "name":"Albert Einstein",
+    "photo":"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSzMy02grOPbnXb0CibPxCkw2h0mBc5E1PuwX6NEEPVRuuezXzy",
+    "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    "name":"Abraham Lincoln",
+    "photo":"http://a4.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTIwNjA4NjMzODg2NTc0MDky.jpg",
+    "scores":[4, 5, 3, 3, 4, 5, 1, 4, 3, 5]
+  },
+  {
+    "name":"George Washington",
+    "photo":"https://www.whitehouse.gov/sites/whitehouse.gov/files/images/first-family/01_george_washington%5B1%5D.jpg",
+    "scores":[3, 4, 2, 2, 3, 4, 5, 3, 2, 4]
+  },
+  {
+    "name":"Leonardo da Vinci",
+    "photo":"http://www.customasapblog.com/wp-content/uploads/2011/11/da-vinci-alam_159842t.jpg",
+    "scores":[ 2, 3, 1, 1, 2, 3, 4, 2, 1, 3]
+  },
+  {
+    "name":"Aristotle",
+    "photo":"http://www.thefamouspeople.com/profiles/thumbs/aristotle-17.jpg",
+    "scores":[ 1, 2, 5, 5, 1, 2, 3, 1, 5, 2]
+  },
+  {
+    "name":"Winston Churchill",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/e/e7/Churchill-in-quebec-1944-23-0201a.gif",
+    "scores":[3, 4, 2, 4, 3, 5, 2, 2, 5, 4]
+  },
+  {
+    "name":"Tom Brady",
+    "photo":"http://dailysnark.com/wp-content/uploads/2016/09/Tom-Brady-1.jpg",
+    "scores": [2, 3, 1, 3, 2, 4, 1, 1, 4, 3]
+  },
+  {
+    "name":"John F. Kennedy",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/5/5e/John_F._Kennedy%2C_White_House_photo_portrait%2C_looking_up.jpg",
+    "scores":[1, 2, 2, 2, 1, 2, 2, 1, 2, 2]
+  },
+  {
+    "name":"Charles Darwin",
+    "photo":"http://a4.files.biography.com/image/upload/c_fill,cs_srgb,dpr_2.0,g_face,h_300,q_80,w_300/MTE5NDg0MDU0OTM4NjE3MzU5.jpg",
+    "scores":[2, 2, 3, 4, 1, 4, 5, 5, 2, 3]
+  },
+  {
+    "name":"Nelson Mandela",
+    "photo":"http://www.idainternational.org/wp-content/uploads/2015/04/Nelson-Mandela.jpg",
+    "scores": [1, 2, 2, 1, 2, 1, 2, 1, 1, 1]
+  }
+  ];
+  
+  
+  //export the friends data so it is accessible in other files using require
+  module.exports = friends;
+  
+  
